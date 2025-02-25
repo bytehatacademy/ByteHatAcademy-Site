9f49a456f2c349367dbf9522257eb81ba413b765
bytehatacademy <bytehatacademy@gmail.com>
2025-02-25 02:53:51 -0800

    Updated path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Matches your repo name—good!
+  base: '/home', // Matches your repo name—good!
   optimizeDeps: {
     exclude: ['lucide-react'], // Fine for dev, no change needed
   },
